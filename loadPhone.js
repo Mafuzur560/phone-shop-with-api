45c7be12ae0cc3526e2bbf38d4cce600c5f23c84
Mafuzur560 <mrridoy461@gmail.com>
2023-08-28 23:35:54 +0600

    Loading spinner added

--- loadPhone.js
+++ loadPhone.js
@@ -37,18 +37,22 @@ const displayPhones = (phones, isShowAll) =>{
         <div class="card-body">
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+            <div class="card-actions justify-center">
+            <button class="btn btn-primary">See death's</button>
             </div>
         </div>       
         `;
         phoneContainer.appendChild(phoneCard);
     });
+
+    // hide loading spinner
+    toggleLoadingSpinner(false);
 }
 
 
 
 const handleSearch = (isShowAll) =>{
+    toggleLoadingSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value ;
     loadPhone(searchText, isShowAll);
@@ -57,4 +61,15 @@ const handleSearch = (isShowAll) =>{
 // Handle show btn
 const handleShowAll = () =>{
     handleSearch(true);
+}
+
+// Loading spinier
+const toggleLoadingSpinner = (isLoading) =>{
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if(isLoading){
+        loadingSpinner.classList.remove('hidden');
+    }else{
+        loadingSpinner.classList.add('hidden');
+    }
+
 }
